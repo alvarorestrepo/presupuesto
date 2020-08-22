e76e3deb73a608d57849e15e56a584aa78631d43
alvarorestrepo <alvarorestrepoz@gmail.com>
2020-08-21 22:52:28 -0500

    terminada la pregunta del presupuesto

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,19 +1,51 @@
-import React, {Fragment} from 'react';
+import React, { Fragment, useState } from 'react';
+import Error from './Error';
 
 const Pregunta = () => {
-    return ( 
+
+    //Definir el state
+    const [cantidad, guardarCantidad] = useState(0);
+    const [error, guardarError] = useState(false);
+
+    //Definir presupuesto
+    const definirPresupuesto = e => {
+        guardarCantidad(parseInt(e.target.value, 10))
+    }
+    //Submit para definir el presupuesto
+    const agregarPresupuesto = e => {
+        e.preventDefault();
+
+        //Validar
+        if (cantidad < 1 || isNaN(cantidad)) {
+            guardarError(true);
+            return;
+        }
+        
+        //Al pasar la validacion
+        guardarError(false);
+    }
+
+    return (
         <Fragment>
             <h2>Coloca tu presupuesto</h2>
 
-            <form action="">
+            {error ? <Error mensaje='El presupuesto es incorrecto'/>: null}
+
+            <form
+                onSubmit={agregarPresupuesto}>
                 <input
-                 type="number" 
-                 className="u-full-width" 
-                 placeholder='Coloca tu presupuesto'/>
+                    type="number"
+                    className="u-full-width"
+                    placeholder='Coloca tu presupuesto'
+                    onChange={definirPresupuesto} />
+                <input
+                    type="submit"
+                    value="Definir Presupuesto"
+                    className='button-primary u-full-width' />
             </form>
         </Fragment>
 
-     );
+    );
 }
- 
+
 export default Pregunta;
